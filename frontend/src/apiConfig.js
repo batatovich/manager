ae0f6b1126b15f426972c22f868a04a7e840a222
Peter <pllaurado@membranelabs.com>
2024-06-14 16:28:46 -0300

    implement axios in api.js

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,13 +1,12 @@
-const API_BASE_URL = 'http://localhost:3000/api'; 
-
 export const API_URLS = {
-  SAVE_ENTRY: `${API_BASE_URL}/entries/save`,
-  GET_ENTRY: `${API_BASE_URL}/entries/get`,
-  EDIT_ENTRY: `${API_BASE_URL}/entries/edit`,
-  DELETE_ENTRY: `${API_BASE_URL}/entries/delete`,
-  GET_ALL_ENTRIES: `${API_BASE_URL}/entries/all`,
-  SAVE_ACCOUNT: `${API_BASE_URL}/accounts/save`,
-  EDIT_ACCOUNT: `${API_BASE_URL}/accounts/edit`,
-  DELETE_ACCOUNT: `${API_BASE_URL}/accounts/delete`,
-  GET_ALL_ACCOUNTS: `${API_BASE_URL}/accounts/all`,
+  BASE: 'http://localhost:3000/api',
+  SAVE_ENTRY: '/entries/save',
+  GET_ENTRY: '/entries/get',
+  EDIT_ENTRY: '/entries/edit',
+  DELETE_ENTRY: '/entries/delete',
+  GET_ALL_ENTRIES: '/entries/all',
+  SAVE_ACCOUNT: '/accounts/save',
+  EDIT_ACCOUNT: '/accounts/edit',
+  DELETE_ACCOUNT: '/accounts/delete',
+  GET_ALL_ACCOUNTS: '/accounts/all',
 };
